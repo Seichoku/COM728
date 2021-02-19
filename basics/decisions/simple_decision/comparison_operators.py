cc7cbb74a1f56272d5bd1c8c2ab28be3a2f3e054
Seichoku <seichoku@gmail.com>
2021-02-19 01:11:45 +0100

    Added code to demonstrate the use of comparison operators.

---
+++ basics/decisions/simple_decision/comparison_operators.py
@@ -0,0 +1,9 @@
+print("Enter your first number")
+first_number = int(input())
+print("Enter second number")
+second_number = int(input())
+if first_number > second_number:
+    print("Second Number is smaller")
+else:
+    print("First number smaller")
+gitadd .
