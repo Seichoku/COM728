df2d1deb4ff5efd63faddc3917392782a46fe475
Seichoku <seichoku@gmail.com>
2021-02-20 16:48:09 +0100

    Added code to demonstrate the use of a user-defined function with multiple parameters.

---
+++ basics/functions/function_with_parameters.py
@@ -0,0 +1,8 @@
+def climb_ladder(steps, left):
+    if steps > left:
+        print("Whew!! We are almost there!!!!! We have to keep moving")
+    else:
+        print("We still have along way to go!!!!!!!!! Hurry")
+
+climb_ladder(5,2)
+climb_ladder(2,21)
