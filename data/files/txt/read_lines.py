6abf878b50c8bc3e305571cdd9fb642458b141c1
Seichoku <seichoku@gmail.com>
2021-03-05 11:39:54 +0100

    Added code to demonstrate a simple list.

---
+++ data/files/txt/read_lines.py
@@ -0,0 +1,15 @@
+
+
+def search(file_name):
+    print("Searching........")
+    with open("library.txt") as file:
+        for location in file:
+            print(f"looked in {location.strip()}")
+
+
+def run():
+    search("library.txt")
+
+
+if __name__ == "__main__":
+    run()
---
+++ data/lists/simple_list.py
@@ -0,0 +1,11 @@
+def direction():
+    directions = ["Move Forward", "Move Backward", "Move left", "Move right"]
+    print(directions)
+
+
+def run():
+    direction()
+
+
+if __name__ == "__main__":
+    run()
